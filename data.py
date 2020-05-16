d83f3fca4f91c805f1e7f75a79b7adb4a91c82d1
Shradhit Subudhi <shradhit@gmail.com>
2020-05-15 22:49:27 -0400

    Update data.py

--- data.py
+++ data.py
@@ -74,120 +74,120 @@ def _process_csv_file(file):
     return files
 
 
-class BalanceCovidDataset(keras.utils.Sequence):
-    'Generates data for Keras'
+# class BalanceCovidDataset(keras.utils.Sequence):
+#     'Generates data for Keras'
 
-    def __init__(
-            self,
-            data_dir,
-            csv_file,
-            is_training=True,
-            batch_size=8,
-            input_shape=(224, 224),
-            n_classes=3,
-            num_channels=3,
-            mapping={
-                'normal': 0,
-                'pneumonia': 1,
-                'COVID-19': 2
-            },
-            shuffle=True,
-            augmentation=apply_augmentation,
-            covid_percent=0.3,
-            class_weights=[1., 1., 6.],
-            top_percent=0.08
-    ):
-        'Initialization'
-        self.datadir = data_dir
-        self.dataset = _process_csv_file(csv_file)
-        self.is_training = is_training
-        self.batch_size = batch_size
-        self.N = len(self.dataset)
-        self.input_shape = input_shape
-        self.n_classes = n_classes
-        self.num_channels = num_channels
-        self.mapping = mapping
-        self.shuffle = True
-        self.covid_percent = covid_percent
-        self.class_weights = class_weights
-        self.n = 0
-        self.augmentation = augmentation
-        self.top_percent = top_percent
+#     def __init__(
+#             self,
+#             data_dir,
+#             csv_file,
+#             is_training=True,
+#             batch_size=8,
+#             input_shape=(224, 224),
+#             n_classes=3,
+#             num_channels=3,
+#             mapping={
+#                 'normal': 0,
+#                 'pneumonia': 1,
+#                 'COVID-19': 2
+#             },
+#             shuffle=True,
+#             augmentation=apply_augmentation,
+#             covid_percent=0.3,
+#             class_weights=[1., 1., 6.],
+#             top_percent=0.08
+#     ):
+#         'Initialization'
+#         self.datadir = data_dir
+#         self.dataset = _process_csv_file(csv_file)
+#         self.is_training = is_training
+#         self.batch_size = batch_size
+#         self.N = len(self.dataset)
+#         self.input_shape = input_shape
+#         self.n_classes = n_classes
+#         self.num_channels = num_channels
+#         self.mapping = mapping
+#         self.shuffle = True
+#         self.covid_percent = covid_percent
+#         self.class_weights = class_weights
+#         self.n = 0
+#         self.augmentation = augmentation
+#         self.top_percent = top_percent
 
-        datasets = {'normal': [], 'pneumonia': [], 'COVID-19': []}
-        for l in self.dataset:
-            datasets[l.split()[2]].append(l)
-        self.datasets = [
-            datasets['normal'] + datasets['pneumonia'],
-            datasets['COVID-19'],
-        ]
-        print(len(self.datasets[0]), len(self.datasets[1]))
+#         datasets = {'normal': [], 'pneumonia': [], 'COVID-19': []}
+#         for l in self.dataset:
+#             datasets[l.split()[2]].append(l)
+#         self.datasets = [
+#             datasets['normal'] + datasets['pneumonia'],
+#             datasets['COVID-19'],
+#         ]
+#         print(len(self.datasets[0]), len(self.datasets[1]))
 
-        self.on_epoch_end()
+#         self.on_epoch_end()
 
-    def __next__(self):
-        # Get one batch of data
-        batch_x, batch_y, weights = self.__getitem__(self.n)
-        # Batch index
-        self.n += 1
+#     def __next__(self):
+#         # Get one batch of data
+#         batch_x, batch_y, weights = self.__getitem__(self.n)
+#         # Batch index
+#         self.n += 1
 
-        # If we have processed the entire dataset then
-        if self.n >= self.__len__():
-            self.on_epoch_end
-            self.n = 0
+#         # If we have processed the entire dataset then
+#         if self.n >= self.__len__():
+#             self.on_epoch_end
+#             self.n = 0
 
-        return batch_x, batch_y, weights
+#         return batch_x, batch_y, weights
 
-    def __len__(self):
-        return int(np.ceil(len(self.datasets[0]) / float(self.batch_size)))
+#     def __len__(self):
+#         return int(np.ceil(len(self.datasets[0]) / float(self.batch_size)))
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle == True:
-            for v in self.datasets:
-                np.random.shuffle(v)
+#     def on_epoch_end(self):
+#         'Updates indexes after each epoch'
+#         if self.shuffle == True:
+#             for v in self.datasets:
+#                 np.random.shuffle(v)
 
-    def __getitem__(self, idx):
-        batch_x, batch_y = np.zeros(
-            (self.batch_size, *self.input_shape,
-             self.num_channels)), np.zeros(self.batch_size)
+#     def __getitem__(self, idx):
+#         batch_x, batch_y = np.zeros(
+#             (self.batch_size, *self.input_shape,
+#              self.num_channels)), np.zeros(self.batch_size)
 
-        batch_files = self.datasets[0][idx * self.batch_size:(idx + 1) *
-                                       self.batch_size]
+#         batch_files = self.datasets[0][idx * self.batch_size:(idx + 1) *
+#                                        self.batch_size]
 
-        # upsample covid cases
-        covid_size = max(int(len(batch_files) * self.covid_percent), 1)
-        covid_inds = np.random.choice(np.arange(len(batch_files)),
-                                      size=covid_size,
-                                      replace=False)
-        covid_files = np.random.choice(self.datasets[1],
-                                       size=covid_size,
-                                       replace=False)
-        for i in range(covid_size):
-            batch_files[covid_inds[i]] = covid_files[i]
+#         # upsample covid cases
+#         covid_size = max(int(len(batch_files) * self.covid_percent), 1)
+#         covid_inds = np.random.choice(np.arange(len(batch_files)),
+#                                       size=covid_size,
+#                                       replace=False)
+#         covid_files = np.random.choice(self.datasets[1],
+#                                        size=covid_size,
+#                                        replace=False)
+#         for i in range(covid_size):
+#             batch_files[covid_inds[i]] = covid_files[i]
 
-        for i in range(len(batch_files)):
-            sample = batch_files[i].split(",")
+#         for i in range(len(batch_files)):
+#             sample = batch_files[i].split(",")
 
-            if self.is_training:
-                folder = 'train'
-            else:
-                folder = 'test'
+#             if self.is_training:
+#                 folder = 'train'
+#             else:
+#                 folder = 'test'
 
-            x = process_image_file(os.path.join(self.datadir, folder, sample[1]),
-                                   self.top_percent,
-                                   self.input_shape[0])
+#             x = process_image_file(os.path.join(self.datadir, folder, sample[1]),
+#                                    self.top_percent,
+#                                    self.input_shape[0])
 
-            if self.is_training and hasattr(self, 'augmentation'):
-                x = self.augmentation(x)
+#             if self.is_training and hasattr(self, 'augmentation'):
+#                 x = self.augmentation(x)
 
-            x = x.astype('float32') / 255.0
-            y = self.mapping[sample[2]]
+#             x = x.astype('float32') / 255.0
+#             y = self.mapping[sample[2]]
 
-            batch_x[i] = x
-            batch_y[i] = y
+#             batch_x[i] = x
+#             batch_y[i] = y
 
-        class_weights = self.class_weights
-        weights = np.take(class_weights, batch_y.astype('int64'))
+#         class_weights = self.class_weights
+#         weights = np.take(class_weights, batch_y.astype('int64'))
 
-        return batch_x, keras.utils.to_categorical(batch_y, num_classes=self.n_classes), weights
+#         return batch_x, keras.utils.to_categorical(batch_y, num_classes=self.n_classes), weights
