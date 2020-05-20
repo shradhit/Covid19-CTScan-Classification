4a6f7315e772f78c9a6839daa1f850c024495f8b
Shradhit Subudhi <shradhit@gmail.com>
2020-05-20 15:17:07 -0400

    Update data.py

--- data.py
+++ data.py
@@ -102,7 +102,8 @@ class BalanceCovidDataset(keras.utils.Sequence):
         x = x.astype('float32') / 255.0
         return x
 
-
+ 
+#chage this please
 transformed_images = BalanceCovidDataset(data_dir='/Users/shradhitsubudhi/Documents/COVID/mywork/modified_data/train/0a51f668-b7b1-4d8d-9ab9-de1f702f071a.png')
 x = transformed_images.transformation()
 print(x.shape)
